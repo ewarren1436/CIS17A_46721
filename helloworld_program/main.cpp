0319b51686b7609349ac2eb568634083b5c45764
ewarren1436 <67296013+ewarren1436@users.noreply.github.com>
2020-06-28 21:57:34 -0700

    Add files via upload

---
+++ helloworld_program/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * File: main.cpp
+ * Author Elizabeth Warren
+ * Created on June 28 2020, 9:45 PM
+ * Purpose Display Hello World To Screen
+ */
+//System Libraries
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+//User Libraries
+//Global Constants -No Global Constants
+//Only Universal constants, Math, Physical, Conversions, Higher Dimensions
+//Function Prototypes
+//Execution Begins Here
+ 
+int main(int argc, char** argv) 
+{
+    //Declare Variable Data Types and Constants
+    //Initialize Variables 
+    //Process or map Inputs to Outputs
+    //Display Outputs
+    cout<<"Hello World";
+    //Exit stage right!   
+
+    return 0;
+}
+
